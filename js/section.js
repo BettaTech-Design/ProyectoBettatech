49cf499d79bb62690788f42fcabbc561562dc7d1
Dchaves <Dachc7107@hotmail.com>
2021-09-30 16:23:34 -0600

    Optimize css

--- js/section.js
+++ js/section.js
@@ -2,7 +2,8 @@
 
 //     let section = document.querySelector('section');
 //     section.innerHTML = `
-//     <div class="contenedor1">
+//     <section id="services" class="about-us" >
+//             <div class="contenedor1">
 //                 <h2 class="titulo">We provide a wide range</h2>
 //                 <div class="contenedor-articulo">
 //                     <div class="articulo" data-aos="zoom-in-right">
@@ -40,8 +41,62 @@
 //                         <h3>Marketing</h3>
 //                         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit autem consequuntur accusamus nulla distinctio, reprehenderit soluta fuga commodi ducimus! Tenetur?</p>
 //                         <a href="#123">Read more </a>
+//                     </div>
 //                 </div>
-//     </div>
+//             </section>
+        
+//         <section>
+//             <div class="questions contenedor" id="questions">
+//                 <section class="contenido-questions">
+//                     <section class="">
+//                         <h1>Our creativity is your success</h1>
+//                         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nam ex laudantium</p>
+//                         <a href="https://www.google.com" target="_bank">Learn more</a>
+//                     </section>
+//                     <img src="img/svg/co-working.svg" alt="" data-aos="zoom-out-right">
+//                 </section>   
+//             </div>
+//                 <section class="results" id="results">
+//                     <div class="contenedor1 content-results">
+//                         <section class="numbers">
+//                             <div class="number" data-aos="zoom-in-left">
+//                                 <h4>2021</h4>
+//                                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa, tempora?</p>
+//                             </div>
+//                             <div class="number" data-aos="zoom-in-left">
+//                                 <h4>2021</h4>
+//                                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa, tempora?</p>
+//                             </div>
+//                             <div class="number" data-aos="zoom-in-left">
+//                                 <h4>2021</h4>
+//                                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa, tempora?</p>
+//                             </div>
+//                             <div class="number" data-aos="zoom-in-left">
+//                                 <h4>2021</h4>
+//                                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa, tempora?</p>
+//                             </div>
+                            
+//                         </section>
+//                         <section class="results-text">
+//                             <h4>Lorem ipsum dolor sit amet.</h4>
+//                             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur sequi vel consectetur.</p>
+//                             <a href="#123">LEARN MORE</a>
+//                         </section>
+//                     </div>
+//                 </section>
+//                 <section class="contenedor1 services" id="support">
+//                     <img src="img/svg/interaction_design.svg" alt="" data-aos="zoom-out-right">
+
+//                     <div class="box-skills">
+//                         <h4><i class="far fa-check-circle"></i> Web Design</h4>
+//                         <h4><i class="far fa-check-circle"></i> Web Development</h4>
+//                         <h4><i class="far fa-check-circle"></i> SEO</h4>
+//                         <h4><i class="far fa-check-circle"></i> Marketing</h4>
+//                         <h4><i class="far fa-check-circle"></i> Web Support</h4>
+//                     </div>
+//                 </section>
+//         </section>
+//     </section>    
 //     `;
 
 // }
