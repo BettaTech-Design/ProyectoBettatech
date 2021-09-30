8af51bfdb6e1a4ab8d44db8a8c0fbcb68b06d8ec
Dchaves <Dachc7107@hotmail.com>
2021-09-30 15:52:48 -0600

    optimize html, movi las etiquetas footer- header- section  a su JS

---
+++ js/header.js
@@ -0,0 +1,34 @@
+const createHeader = () => {
+
+    let header = document.querySelector('header');
+    header.innerHTML = `
+    <nav>
+        <section class="contenedor nav">
+            <div class="logo">
+                <img src="img/png/tecnología.png" alt="BettaTech">
+            </div>
+            <div class="enlaces-header">
+                <a href="#inicio">Inicio</a>
+                <a href="#services">Services</a>
+                <a href="#results">Results</a>
+                <a href="#support">Support</a>
+                <a href="#contact">Contact</a>
+            </div>
+            <div class="hamburguer">
+                <i class="fas fa-bars"></i>
+            </div>
+        </section>
+    </nav>
+        <div class="questions contenedor" id="inicio">
+            <section class="contenido-header">
+                <section class="textos-header">
+                    <h1>Our creativity is your success</h1>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nam ex laudantium</p>
+                    <a href="https://www.google.com" target="_bank">Learn more</a>
+                </section>
+                    <img src="img/svg/1.svg" alt="">
+            </section>
+        </div>`;
+}
+
+createHeader();
---
+++ js/section.js
@@ -0,0 +1,49 @@
+// const createSection = () => {
+
+//     let section = document.querySelector('section');
+//     section.innerHTML = `
+//     <div class="contenedor1">
+//                 <h2 class="titulo">We provide a wide range</h2>
+//                 <div class="contenedor-articulo">
+//                     <div class="articulo" data-aos="zoom-in-right">
+//                         <i class="fas fa-pen-fancy"></i>
+//                         <h3>Graphic Design</h3>
+//                         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit autem consequuntur accusamus nulla distinctio, reprehenderit soluta fuga commodi ducimus! Tenetur?</p>
+//                         <a href="https://www.google.com" target="_bank">Read more </a>
+//                     </div>
+//                     <div class="articulo" data-aos="zoom-in-right">
+//                         <i class="fas fa-code"></i>
+//                         <h3>Web Design</h3>
+//                         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit autem consequuntur accusamus nulla distinctio, reprehenderit soluta fuga commodi ducimus! Tenetur?</p>
+//                         <a href="#123">Read more </a>
+//                     </div>
+//                     <div class="articulo" data-aos="zoom-in-right">
+//                         <i class="fas fa-cogs"></i>
+//                         <h3>Video Production</h3>
+//                         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit autem consequuntur accusamus nulla distinctio, reprehenderit soluta fuga commodi ducimus! Tenetur?</p>
+//                         <a href="#123">Read more </a>
+//                     </div>
+//                     <div class="articulo" data-aos="zoom-in-right">
+//                         <i class="fas fa-chart-line"></i>
+//                         <h3>Inphografics</h3>
+//                         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit autem consequuntur accusamus nulla distinctio, reprehenderit soluta fuga commodi ducimus! Tenetur?</p>
+//                         <a href="#123">Read more </a>
+//                     </div>
+//                     <div class="articulo" data-aos="zoom-in-right">
+//                         <i class="fas fa-chart-pie"></i>
+//                         <h3>SEO</h3>
+//                         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit autem consequuntur accusamus nulla distinctio, reprehenderit soluta fuga commodi ducimus! Tenetur?</p>
+//                         <a href="#123">Read more </a>
+//                     </div>
+//                     <div class="articulo" data-aos="zoom-in-right">
+//                         <i class="far fa-comments"></i>
+//                         <h3>Marketing</h3>
+//                         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit autem consequuntur accusamus nulla distinctio, reprehenderit soluta fuga commodi ducimus! Tenetur?</p>
+//                         <a href="#123">Read more </a>
+//                 </div>
+//     </div>
+//     `;
+
+// }
+
+// createSection();
