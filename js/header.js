0e12f98c66a03dab514c8c52854257ef3ac44bb3
dchaves <dachc7107@hotmail.com>
2021-10-19 14:39:02 -0600

    1234

--- js/header.js
+++ js/header.js
@@ -5,11 +5,7 @@ const createHeader = () => {
     <nav>
         <section class="contenedor nav">
             <div class="logo">
-<<<<<<< HEAD
-                <img src="img/png/tecnología.png" alt="BettaTech">
-=======
                 <img src="img/png/tecnologia.png" alt="BettaTech">
->>>>>>> 911c255bf74968891e2eee986cf4e2ee64af439b
             </div>
             <div class="enlaces-header">
                 <a href="#inicio">Inicio</a>
@@ -26,13 +22,8 @@ const createHeader = () => {
         <div class="questions contenedor" id="inicio">
             <section class="contenido-header">
                 <section class="textos-header">
-<<<<<<< HEAD
-                    <h1>Our creativity is your success</h1>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim, similique rerum.</p>
-=======
-                    <h1>Buenas tardes Marianela Chaves</h1>
-                    <p>Este es mi pequeño sitio wed, si no te gusta te comer un tufi xD xD </p>
->>>>>>> 911c255bf74968891e2eee986cf4e2ee64af439b
+                    <h1>Lorem ipsum dolor sit amet</h1>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur, animi nostrum?</p>
                     <a href="https://www.google.com" target="_bank">Learn more</a>
                 </section>
                     <img src="img/svg/1.svg" alt="">
